320de15f1b09e2a8c33668caefc994c9faf3df7e
Duda Santos <maria.oliveira@zup.com.br>
2024-08-12 11:07:31 -0300

    add io class

--- src/IO.java
+++ src/IO.java
@@ -1,3 +1,13 @@
-public class IO {
+import java.util.Scanner;
 
+public class IO {
+        public double receberDados(String message){
+            System.out.println(message);
+            return new Scanner(System.in).nextDouble();
+        }
+
+        public void exibirDados(double result){
+            System.out.println("O resultado da sua conta é: "+result);
+
+        }
 }
