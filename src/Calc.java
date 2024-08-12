dd04763c5d46bfb045c8e07ea8448e10fff96143
Duda Santos <maria.oliveira@zup.com.br>
2024-08-12 11:03:45 -0300

    first commit

---
+++ src/Calc.java
@@ -0,0 +1,17 @@
+public class Calc {
+    public double soma(double a, double b){
+        return a + b;
+    }
+
+    public double subtracao(double a, double b){
+        return a - b;
+    }
+
+    public double multiplicacao(double a, double b){
+        return a * b;
+    }
+
+    public double divisao(double a, double b){
+        return a / b;
+    }
+}
